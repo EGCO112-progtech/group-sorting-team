61954b76d5df274e8c48290bcce45ad2cbf90c18
Wichpol Vitayakomol <123149025+act22971@users.noreply.github.com>
2023-02-03 09:43:02 +0000

    attemp01

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #define N 6
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "sorting.h"
 
-int main() {
-  int a[N]={3,2,6,7,3,1};
-  int i,j,new_number;
-
-   display(a,N);
-  bubbleSort(a,N); 
-   //insertion(a,N);
-
-  // selectionSort(a,N);
-   display(a,N);
- return 0;
+int main(int argc ,char *argv[]) {
+  int i,*a,N;
+int j=0,new_number;
+N=argc-1;
+a=(int*)malloc(sizeof(int)*N);
+int *prime=(int*)malloc(sizeof(int)*N);
+for(i=0;i<N;i++) {
+a[i]=atoi(argv[i+1]);
+  if(strcmp(argv[2],"bubble")==0){display(a,N); bubbleSort(a,N); display(a,N);}
+  else if(strcmp(argv[2],"insertion")==0){display(a,N); insertion(a,N); display(a,N);}
+  else if(strcmp(argv[2],"selectionsort")==0){display(a,N); selectionSort(a,N); display(a,N);}
+  else printf("Bruh");
+  return 0;
 }
 
 
